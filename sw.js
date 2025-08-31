d983731d03df8a997a9366cfa69f4d314f41b39a
Kh2dem <61298638+Kh2dem@users.noreply.github.com>
2025-08-31 06:48:24 +0400

    sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,14 @@
 const CACHE = "arcade-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest"
-  // أضف أيقوناتك هنا: "icon-192.png","icon-512.png"
-];
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
 
 self.addEventListener("activate", e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))) );
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
   self.clients.claim();
 });
 
